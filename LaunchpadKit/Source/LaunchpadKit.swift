80fce9ca5f50e7e1f2a9c02ae2cbac29680f7dd8
cemolcay <ccemolcay@gmail.com>
2019-05-27 17:28:29 +0300

    Add documentation

--- LaunchpadKit/Source/LaunchpadKit.swift
+++ LaunchpadKit/Source/LaunchpadKit.swift
@@ -10,36 +10,56 @@ import Foundation
 import AudioKit
 import MIDIEventKit
 
+/// Represents a button on Launchpad.
 public class LaunchpadButton {
+  /// Brightness level of a button.
   public enum Brightness: Int {
+    /// No brightness.
     case off
+    /// Minimum brightness.
     case min
+    /// Medium brightness.
     case mid
+    /// Maximum brightness.
     case max
   }
 
+  /// Color of the button.
   public enum Color: Int {
+    /// Green color.
     case green
+    /// Red color.
     case red
+    /// Yellow color.
     case yellow
+    /// Amber color.
     case amber
   }
 
-  public let x: Int
-  public let y: Int
+  /// Row index of the button on the grid.
+  public let col: Int
+  /// Column index of the button on the grid.
+  public let row: Int
+  /// MIDI note value of the button on the grid.
   public let midiNote: UInt8
+  /// Current color of the button.
   public internal(set) var color: Color
+  /// Current brightness of the button.
   public internal(set) var brightness: Brightness
+  /// Current pressing state of the button.
   public internal(set) var isPressed: Bool
 
+  /// Returns true if the button is located on top row.
   public var isLiveButton: Bool {
-    return y == 0
+    return row == 0
   }
 
+  /// Returns true if the buttons is located on last column.
   public var isSceneButton: Bool {
-    return x == 8
+    return col == 8
   }
 
+  /// Returns the MIDI code for the current color/brightness configuration.
   public var colorCode: UInt8 {
     var red = brightness.rawValue * (color == .red || color == .amber ? 1 : 0)
     var green = brightness.rawValue * (color == .red || color == .amber ? 1 : 0)
@@ -54,44 +74,70 @@ public class LaunchpadButton {
       UInt8(0b00001 * red)
   }
 
+  /// Returns the MIDI status code for the color message sending.
   public var colorStatusCode: UInt8 {
     return isLiveButton ? 176 : 144
   }
 
-  public init(x: Int, y: Int, color: Color = .green, brightness: Brightness = .off) {
-    self.x = x
-    self.y = y
+  /// Initilizes the button with
+  ///
+  /// - Parameters:
+  ///   - row: Row index, y-position of the button.
+  ///   - col: Column index, x-position of the button.
+  ///   - color: Color of the button.
+  ///   - brightness: Brightness of the button.
+  public init(row: Int, col: Int, color: Color = .green, brightness: Brightness = .off) {
+    self.row = row
+    self.col = col
     self.color = color
     self.brightness = brightness
     self.isPressed = false
-    self.midiNote = y == 8 ? UInt8(104 + x) : UInt8((y * 16) + x)
+    self.midiNote = col == 8 ? UInt8(104 + col) : UInt8((row * 16) + col)
   }
 }
 
+/// Represents a connected Launchpad.
 public class Launchpad {
+  /// Contains all the buttons on launchpad.
   public var grid: [LaunchpadButton]
+  /// MIDI-input name of the launchpad.
   public internal(set) var name: String
+  /// MIDI port of the launchpad.
   public internal(set) var port: MIDIUniqueID
+  /// Current connection state of the launchpad.
   public internal(set) var isConnected: Bool
 
+  /// Initilizes a Launchpad with it's MIDI configuration.
+  ///
+  /// - Parameters:
+  ///   - name: MIDI-input name of the Launchpad.
+  ///   - port: MIDI port of the Launchpad.
   public init(name: String, port: MIDIUniqueID) {
-    grid = [Int](0..<8).map({ x in [Int](0..<8).map({ y in LaunchpadButton(x: x, y: y) }) }).flatMap({ $0 })
+    grid = [Int](0..<8).map({ x in [Int](0..<8).map({ y in LaunchpadButton(row: y, col: x) }) }).flatMap({ $0 })
     self.name = name
     self.port = port
     isConnected = false
   }
 
+  /// Gets or sets a `LaunchpadButton` on the grid by its x-y position.
+  ///
+  /// - Parameters:
+  ///   - x: Column of the button on the grid.
+  ///   - y: Row of the button on the grid.
   public subscript(_ x: Int, _ y: Int) -> LaunchpadButton? {
     get {
-      return grid.first(where: { $0.x == x && $0.y == y })
+      return grid.first(where: { $0.col == x && $0.row == y })
     } set {
-      guard let index = grid.firstIndex(where: { $0.x == x && $0.y == y }),
+      guard let index = grid.firstIndex(where: { $0.col == x && $0.row == y }),
         let button = newValue
         else { return }
       grid[index] = button
     }
   }
 
+  /// Gets or sets a `LaunchpadButton` on the grid by it's MIDI note number.
+  ///
+  /// - Parameter number: MIDI note number of the button on the grid.
   public subscript(midiNote number: UInt8) -> LaunchpadButton? {
     get {
       return grid.first(where: { $0.midiNote == number })
@@ -103,44 +149,97 @@ public class Launchpad {
     }
   }
 
-  public subscript(row index: Int) -> [LaunchpadButton] {
-    return grid.filter({ $0.x == index })
-  }
-
+  /// Returns all buttons on a column.
+  ///
+  /// - Parameter index: Index of the column.
   public subscript(col index: Int) -> [LaunchpadButton] {
-    return grid.filter({ $0.y == index })
+    return grid.filter({ $0.col == index })
   }
 
+  /// Returns all buttons on a row.
+  ///
+  /// - Parameter index: Index of the row.
+  public subscript(row index: Int) -> [LaunchpadButton] {
+    return grid.filter({ $0.row == index })
+  }
+
+  /// Returns scene buttons on the grid.
   public var sceneButtons: [LaunchpadButton] {
     return grid.filter({ $0.isSceneButton })
   }
 
+  /// Returns live buttons on the grid.
   public var liveButtons: [LaunchpadButton] {
     return grid.filter({ $0.isLiveButton })
   }
 }
 
+/// Informs implementor about Launchpad actions.
 public protocol LaunchpadManagerDelegate: class {
+  /// Informs about Launchpad device changes.
+  ///
+  /// - Parameter launchpadManager: Manager object.
   func launchpadManagerSetupDidChange(_ launchpadManager: LaunchpadManager)
+
+  /// Informs that a Launchpad is connected.
+  ///
+  /// - Parameters:
+  ///   - launchpadManager: Manager object.
+  ///   - launchpad: Connected Launchpad.
   func launchpadManager(_ launchpadManager: LaunchpadManager, didConnect launchpad: Launchpad)
+
+  /// Informs that a Launchpad is disconnected.
+  ///
+  /// - Parameters:
+  ///   - launchpadManager: Manager object.
+  ///   - launchpad: Disconnected Launchpad.
   func launchpadManager(_ launchpadManager: LaunchpadManager, didDisconnect launchpad: Launchpad)
+
+  /// Informs that a button has pressed on a Launchpad.
+  ///
+  /// - Parameters:
+  ///   - launchpadManager: Manager object.
+  ///   - launchpad: Launchpad object.
+  ///   - button: Pressed button.
   func launchpadManager(_ launchpadManager: LaunchpadManager, launchpad: Launchpad, didPress button: LaunchpadButton)
+
+  /// Informs that a button is unpressed on a Launchpad.
+  ///
+  /// - Parameters:
+  ///   - launchpadManager: Manager object.
+  ///   - launchpad: Launchpad object.
+  ///   - button: Unpressed button.
   func launchpadManager(_ launchpadManager: LaunchpadManager, launchpad: Launchpad, didUnpress button: LaunchpadButton)
 }
 
+/// Singleton manager object.
 public class LaunchpadManager: AKMIDIListener {
+  /// Shared singleton object.
   public static let shared = LaunchpadManager()
+  /// MIDI manager.
   private let midi = AKMIDI()
+  /// Currently available Launchpad devices.
   public private(set) var launchpads: [Launchpad] = []
+  /// If enables, auto connects the plugged Launchpads. Defaults true.
   public var autoconnectEnabled = true
+  /// Delegate that informs about changes.
   public weak var delegate: LaunchpadManagerDelegate?
 
+  /// Initilizes the manager.
   private init() {
     midi.addListener(self)
   }
 
   // MARK: Launchpad Color Setting
 
+  /// Sets a launchpad's button color.
+  ///
+  /// - Parameters:
+  ///   - launchpad: Launchpad object.
+  ///   - x: Column of the button on the grid.
+  ///   - y: Row of the button on the grid.
+  ///   - color: Color of the button.
+  ///   - brightness: Brightness of the button.
   public func setLaunchpadButtonColor(_ launchpad: Launchpad,
                                       x: Int,
                                       y: Int,
@@ -156,6 +255,9 @@ public class LaunchpadManager: AKMIDIListener {
 
   // MARK: Launchpad Connection
 
+  /// Connects a Launchpad.
+  ///
+  /// - Parameter launchpad: Connecting Launchpad.
   public func connect(launchpad: Launchpad) {
     midi.openInput(uid: launchpad.port)
     midi.openOutput(uid: launchpad.port)
@@ -163,6 +265,9 @@ public class LaunchpadManager: AKMIDIListener {
     delegate?.launchpadManager(self, didConnect: launchpad)
   }
 
+  /// Disconnects from a Launchpad.
+  ///
+  /// - Parameter launchpad: Disconnecting Launchpad.
   public func disconnect(launchpad: Launchpad) {
     midi.closeInput(uid: launchpad.port)
     midi.closeOutput(uid: launchpad.port)

a198f5dfe76fc9ae027c45b5f8e426a5c244c7e0
cemolcay <ccemolcay@gmail.com>
2019-05-27 12:36:09 +0300

    Initial Commit

---
+++ LaunchpadKit/LaunchpadKit.h
@@ -0,0 +1,19 @@
+//
+//  LaunchpadKit.h
+//  LaunchpadKit
+//
+//  Created by cem.olcay on 27/05/2019.
+//  Copyright © 2019 cemolcay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaunchpadKit.
+FOUNDATION_EXPORT double LaunchpadKitVersionNumber;
+
+//! Project version string for LaunchpadKit.
+FOUNDATION_EXPORT const unsigned char LaunchpadKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchpadKit/PublicHeader.h>
+
+
